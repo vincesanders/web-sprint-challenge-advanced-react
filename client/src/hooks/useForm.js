1f87bceecc57f920ce8be5990d250ed9d421cc65
Vincent Sanders <vincesanders@hotmail.com>
2020-08-16 18:18:10 -0500

    Built useForm and changed logic in checkout form to conform.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = initialValue => {
+    const [formState, setFormState] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...formState, [e.target.name]: e.target.value });
+    };
+
+    return [formState, handleChanges];
+}
+
+export default useForm;
